ab18937b1186c734a613faaca9b0bb2d2b82956c
Gongzai Li <gli65@uclive.ac.nz>
2021-03-04 18:00:52 +1300

    S302T20-12 #time 1h 12m Implemented Show message if login fields are empty. ?implement

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,39 +1,62 @@
 <template>
-  <div>
+  <div id="loginPage">
     <h2>Login to Wasteless</h2>
     <p>Email</p>
-    <input v-model="email"
-        size="30"
-        autofocus
-        autocomplete="off"
+    <input type="email" v-model="email" required
+           size="30"
+           autofocus
+           autocomplete="off"
     />
     <p>Password</p>
-    <input v-model="password"
-        size="30"
-        autofocus
-        autocomplete="off"
+    <input v-model="password" type="password" required
+           size="30"
+           autofocus
+           autocomplete="off"
     /> <br>
-    <button v-on:click="login"
-      style="margin-top:10px">Login</button>
+
+    <div v-if="errors.length">
+      <ul>
+        <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+      </ul>
+    </div>
+
+    <button type='submit' v-on:click="login"
+            style="margin-top:10px">Login
+    </button>
   </div>
 
 </template>
 
 <script>
+
 module.exports = {
-  data: function() {
+  data: function () {
     return {
-      email: "",
-      password: "",
+      errors: [],
+      email: null,
+      password: ""
     }
   },
   methods: {
-    login: function() {
+    login: function () {
+      if (this.email && this.password) {
+        return true;
+      }
+      this.errors = [];
+
+      if (!this.email) {
+        this.errors.push("Please Fill In The Email Field");
+      }
+      if (!this.password) {
+        this.errors.push("Please Fill In The Password Field");
+      }
+
       console.log({
-        email: this.email, 
-        password: this.password
+        email: this.email,
+        password: this.password,
       });
     }
   }
 }
 </script>
+
