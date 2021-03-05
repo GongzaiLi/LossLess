468cbba10f515021bc14fe758241b3ca8fbc96cf
Eric Song <eso27@uclive.ac.nz>
2021-03-05 15:13:38 +1300

    #S302T20-18 #time 20m Profile page layout improvements - Bold property names - Horizontal lines around profile info ?implement ?pair[eso27, gli65] #review

--- frontend/src/components/UserProfile.vue
+++ frontend/src/components/UserProfile.vue
@@ -2,21 +2,21 @@
   <div id="userBox">
     <h2>{{ userDetail.nickname }}'s Profile Page</h2>
     <div>
-      <p>Member since: {{ dateR.day + " " + dateR.month[0] + " " + dateR.year + " (" + registrationTime + ") " }}</p>
+      <p id="member-since">Member since: {{ dateR.day + " " + dateR.month[0] + " " + dateR.year + " (" + registrationTime + ") " }}</p>
     </div>
-
+    <hr>
     <div>
-      <p>Name: {{ userDetail.firstName + " " + userDetail.middleName + userDetail.lastName }}</p>
-      <p>Date Of Birth: {{ userDetail.dateOfBirth }}</p>
-      <p>Email: {{ userDetail.email }}</p>
-      <p>Phone Number: {{ userDetail.phoneNumber }}</p>
-      <p>Home Address: {{ userDetail.homeAddress }}</p>
-      <p>Describe: {{ userDetail.bio }}</p>
+      <p><b>Name:</b> {{ userDetail.firstName + " " + userDetail.middleName + " " + userDetail.lastName }}</p>
+      <p><b>Date Of Birth:</b> {{ userDetail.dateOfBirth }}</p>
+      <p><b>Email:</b> {{ userDetail.email }}</p>
+      <p><b>Phone Number:</b> {{ userDetail.phoneNumber }}</p>
+      <p><b>Home Address:</b> {{ userDetail.homeAddress }}</p>
+      <p><b>Bio:</b> {{ userDetail.bio }}</p>
     </div>
 
+    <hr>
 
     <button id="buttonLog" @click="logOut" style="margin-top:10px">Log out</button>
-
 
   </div>
 
@@ -33,15 +33,15 @@ export default {
       },
       registrationTime: 0,
       userDetail: {
-        firstName: "",
-        lastName: "",
-        middleName: " ",
+        firstName: "Firstname",
+        lastName: "Lastname",
+        middleName: "Middlename",
         nickname: "Someone",
-        bio: "",
-        email: "",
-        dateOfBirth: "",
-        phoneNumber: "",
-        homeAddress: "",
+        bio: "Likes long walks on the beach",
+        email: "johnsmith99@gmail.com",
+        dateOfBirth: "1999-04-27",
+        phoneNumber: "+64 3 555 0129",
+        homeAddress: "4 Rountree Street, Upper Riccarton",
 
       }
 
@@ -61,8 +61,12 @@ export default {
   border: 2px solid #a1a1a1;
   padding: 10px 40px;
   background: floralwhite;
-  width: 300px;
+  width: 400px;
   border-radius: 10px;
+}
+
+#member-since {
+  margin-top: -10px;
 }
 
 </style>
