8c838e5f453208d16b3e9e25579969ab1924c70d
cws47 <cws47@uclive.ac.nz>
2021-03-07 16:06:38 +1300

    S302T20-34 #time 20m added searchbar, search button and basic table with some dummy data ?implement

--- frontend/src/components/UserSearch.vue
+++ frontend/src/components/UserSearch.vue
@@ -7,12 +7,52 @@ Date: 7/3/2021
   <div>
     <h2>Search For a User</h2>
 
+    <input type="search" v-model="searchQuery" size="30" autofocus />
+    <button> Search </button>
+
+    <br><br><br>
+    <table style="width: 100%" >
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>City</th>
+        <th>Country</th>
+        <th>Email</th>
+
+      </tr>
+      <tr>
+        <td>Jill</td>
+        <td>Smith</td>
+        <td>Christchurch</td>
+        <td>New Zealand</td>
+        <td>jill@gmail.com</td>
+      </tr>
+      <tr>
+        <td>Eve</td>
+        <td>Jackson</td>
+        <td>Dunedin</td>
+        <td>New Zealand</td>
+        <td>Eve@gmail.com</td>
+      </tr>
+      <tr>
+        <td>Bob</td>
+        <td>Jones</td>
+        <td>Sydney</td>
+        <td>Australia</td>
+        <td>Bob@outlook.com</td>
+      </tr>
+    </table>
+
   </div>
 </template>
 
 <script>
 export default {
-name: "UserSearch"
+  data: function () {
+    return {
+      searchQuery: ""
+    }
+  }
 }
 </script>
 
