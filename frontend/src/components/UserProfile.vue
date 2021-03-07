fb79f5f771518b06b3703e581f8eb6a53031f270
Gongzai Li <gli65@uclive.ac.nz>
2021-03-07 13:05:27 +1300

    S302T20-19 #time 6m review the task and using userDate attribute in the HTML instead of userDetail attribute.
    ?fix ?review

--- frontend/src/components/UserProfile.vue
+++ frontend/src/components/UserProfile.vue
@@ -5,19 +5,19 @@ Date: 5/3/2021
 -->
 <template>
   <div id="userBox">
-    <h2>{{ userDetail.nickname }}'s Profile Page</h2>
+    <h2>{{ userData.nickname }}'s Profile Page</h2>
     <div>
       <p id="member-since">Member since:
         {{ dateR.day + " " + dateR.month[0] + " " + dateR.year + " (" + registrationTime + ") " }}</p>
     </div>
     <hr>
     <div>
-      <p><b>Name:</b> {{ userDetail.firstName + " " + userDetail.middleName + " " + userDetail.lastName }}</p>
-      <p><b>Date Of Birth:</b> {{ userDetail.dateOfBirth }}</p>
-      <p><b>Email:</b> {{ userDetail.email }}</p>
-      <p><b>Phone Number:</b> {{ userDetail.phoneNumber }}</p>
-      <p><b>Home Address:</b> {{ userDetail.homeAddress }}</p>
-      <p><b>Bio:</b> {{ userDetail.bio }}</p>
+      <p><b>Name:</b> {{ userData.firstName + " " + userData.middleName + " " + userData.lastName }}</p>
+      <p><b>Date Of Birth:</b> {{ userData.dateOfBirth }}</p>
+      <p><b>Email:</b> {{ userData.email }}</p>
+      <p><b>Phone Number:</b> {{ userData.phoneNumber }}</p>
+      <p><b>Home Address:</b> {{ userData.homeAddress }}</p>
+      <p><b>Bio:</b> {{ userData.bio }}</p>
     </div>
 
     <hr>
@@ -42,7 +42,7 @@ export default {
         day: 0
       },
       registrationTime: 0,
-      userDetail: {
+      userData: {
         firstName: "",
         lastName: "",
         middleName: "",
@@ -52,9 +52,7 @@ export default {
         dateOfBirth: "",
         phoneNumber: "",
         homeAddress: ""
-      },
-      userData: {}
-
+      }
     }
   },
 
@@ -77,17 +75,17 @@ export default {
             this.$log.debug(error);
             this.error = "Failed to Load User Date"
           })
-      this.userData = usersInfo.users[0]; // fake the Api data from the response data
 
-      this.userDetail.firstName = this.userData.firstName;
-      this.userDetail.lastName = this.userData.lastName;
-      this.userDetail.middleName = this.userData.middleName;
-      this.userDetail.nickname = this.userData.nickname;
-      this.userDetail.bio = this.userData.bio;
-      this.userDetail.email = this.userData.email;
-      this.userDetail.dateOfBirth = this.userData.dateOfBirth;
-      this.userDetail.phoneNumber = this.userData.phoneNumber;
-      this.userDetail.homeAddress = this.userData.homeAddress;
+      // fake the Api data from the response data
+      this.userData.firstName = usersInfo.users[0].firstName;
+      this.userData.lastName = usersInfo.users[0].lastName;
+      this.userData.middleName = usersInfo.users[0].middleName;
+      this.userData.nickname = usersInfo.users[0].nickname;
+      this.userData.bio = usersInfo.users[0].bio;
+      this.userData.email = usersInfo.users[0].email;
+      this.userData.dateOfBirth = usersInfo.users[0].dateOfBirth;
+      this.userData.phoneNumber = usersInfo.users[0].phoneNumber;
+      this.userData.homeAddress = usersInfo.users[0].homeAddress;
     }
 
 
