cb43ad420dcd464169c99e5f151b21df94f6462c
Eric Song <eso27@uclive.ac.nz>
2021-03-11 17:54:52 +1300

    #S302T20-20 #time 1h 38m Improve accuracy of months and years elapsed calculation in profile page ?implement ?test ?document - New calculation based on days, not months (so a year has 365 days instead of 360) - Tried mocking the current date and time, but was unsuccessful

--- frontend/src/components/UserProfile.vue
+++ frontend/src/components/UserProfile.vue
@@ -36,7 +36,7 @@ export default {
         dateOfBirth: "1999-04-27",
         phoneNumber: "+64 3 555 0129",
         homeAddress: "4 Rountree Street, Upper Riccarton",
-        created: "2020-07-14T14:32:00Z",
+        created: "2021-03-11T00:32:00Z",
       }
 
     }
@@ -44,24 +44,43 @@ export default {
   methods: {
     logOut: function () {
 
-    }
+    },
+    /**
+     * Takes a starting and ending date, then calculates the integer number of years and months elapsed since that date.
+     * The months elapsed is not the total number of months elapsed, but the number months elapsed in
+     * addition to the years also elapsed. For example, 1 year and 2 months instead of 1 year, 14 months.
+     * Assumes that a year is 365 days, and every month is exactly 1/12 of a year.
+     * Returns data in the format {months: months_elapsed, years: years_elapsed}
+     */
+    getMonthsAndYearsBetween (start, end) {
+      const timeElapsed = end - start;
+      const daysElapsed = Math.floor(timeElapsed / (1000 * 60 * 60 * 24));
+      const yearsElapsed = Math.floor(daysElapsed / 365);
+      console.log(daysElapsed / 365);
+      const monthsElapsed = Math.floor(((daysElapsed / 365) - yearsElapsed) * 12);
+      return {
+        months: monthsElapsed,
+        years: yearsElapsed
+      }
+    },
   },
   computed: {
     memberSince: function() {
       let registeredDate = new Date(this.userDetail.created);
-      let registeredMonths = Math.round((Date.now() - registeredDate) / (1000 * 60 * 60 * 24 * 30));
-      let registeredYears = Math.round(registeredMonths / 12);
+      const timeElapsed = this.getMonthsAndYearsBetween(registeredDate, Date.now());
+      const registeredYears = timeElapsed.years;
+      const registeredMonths = timeElapsed.months;
 
       const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
       let message = "Member since: " + registeredDate.getDate() + " " + months[registeredDate.getMonth()] + " " + registeredDate.getFullYear() + " (";
       if (registeredYears > 0) {
-        message += registeredYears + ((registeredYears > 1) ? " Years" : " Year");
+        message += registeredYears + ((registeredYears > 1) ? " Year" : " Years");
         if (registeredMonths > 0) {
           message += " and ";
         }
       }
-      if (registeredMonths > 0) {
-        message += registeredMonths + ((registeredMonths > 1) ? " Months" : " Month");
+      if (registeredMonths > 0 || registeredYears === 0) {
+        message += registeredMonths + ((registeredMonths === 1) ? " Month" : " Months");
       }
       return message + ") ";
     }
