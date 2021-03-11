7612060bd39a95ce67608f34be8cbb1618feae09
Oliver Cranshaw <ojc31@uclive.ac.nz>
2021-03-12 12:59:34 +1300

    S302T8-22 #time 30m Fixed an issue with validating the request body of a login ?fix

---
+++ backend/src/main/java/org/seng302/example/User/Login.java
@@ -0,0 +1,20 @@
+package org.seng302.example.User;
+
+import lombok.Data;
+import lombok.ToString;
+import javax.persistence.Column;
+import javax.validation.constraints.NotNull;
+
+@Data // generate setters and getters for all fields (lombok pre-processor)
+@ToString // generate a toString method
+public class Login {
+
+    @NotNull(message = "email is mandatory")
+    private String email;
+
+    @NotNull(message = "password is mandatory")
+    private String password;
+
+}
+
+
