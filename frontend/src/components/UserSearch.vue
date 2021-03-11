415349ddb1a2de5d6cf075df2dd5a5fa33a84631
Gongzai Li <gli65@uclive.ac.nz>
2021-03-11 19:44:02 +1300

    S302T20-37 #time 1h 46m implement Sort data when table headers clicked - using the Bootstrap to implement the table, when click the table header the table will sort the data. - also implement the ascending and descending. ?implement

--- frontend/src/components/UserSearch.vue
+++ frontend/src/components/UserSearch.vue
@@ -6,15 +6,19 @@ Date: 7/3/2021
 <template>
   <div>
     <h2>Search For a User</h2>
+    <form>
+      <div>
+        <input type="search" v-model="searchQuery" s ize="30" autofocus/>
+        <button @click="displayResults"> Search </button>
+      </div>
+      <br><br><br>
+    </form>
 
-    <input type="search" v-model="searchQuery" size="30" autofocus />
-    <button @click="displayResults"> Search </button>
-
-
-    <br><br><br>
     <div>
-      <b-table striped hover :items="items" style="width: fit-content"></b-table>
+      <b-table sticky-header striped hover :fields="fields" :items="items" style="width: fit-content"></b-table>
+      <b-pagination></b-pagination>>
     </div>
+
   </div>
 </template>
 
@@ -26,7 +30,61 @@ export default {
   data: function () {
     return {
       searchQuery: "",
-      items: []
+
+      items: [],
+      fields: [
+
+        {
+          key: ['id', 'firstName'],
+          sortable: true
+        },
+
+        {
+          key: 'lastName',
+          sortable: true
+        },
+        {
+          key: 'middleName',
+          sortable: true
+        },
+        {
+          key: 'nickname',
+          sortable: true
+        },
+        {
+          key: 'bio',
+          sortable: true
+        },
+        {
+          key: 'email',
+          sortable: true
+        },
+        {
+          key: 'dateOfBirth',
+          sortable: true
+        },
+        {
+          key: 'phoneNumber',
+          sortable: true
+        },
+        {
+          key: 'homeAddress',
+          sortable: true
+        },
+        {
+          key: 'created',
+          sortable: true
+        },
+        {
+          key: 'role',
+          sortable: true
+        },
+        {
+          key: 'businessesAdministered',
+          sortable: true
+        },
+      ]
+
     }
   },
   methods: {
@@ -46,14 +104,18 @@ export default {
           this.$log.debug(error);
           this.error = "Failed to load user data";
         })
+      console.log(this.items);
     }
+  },
+  computed: {
+
   }
 }
 </script>
 
 <style>
 table, th, td {
-  border: 1px solid black;
+  border: 2px solid black;
   border-collapse: collapse;
   padding: 5px;
   text-align: left;
