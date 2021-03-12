1cdd6ade5765cc406db1fe497073870ebb1e9efc
Eric Song <eso27@uclive.ac.nz>
2021-03-12 16:37:48 +1300

    S302T20-69 #time 32m Create Bootstrap navbar with logout button ?implement ?document

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,22 @@
 <template>
   <b-container id="app">
+    <b-navbar toggleable="lg" type="dark" variant="dark" fixed="top">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown right>
+            <template #button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item @click="logOut">Log Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
     <router-view/>
   </b-container>
 </template>
@@ -24,6 +41,15 @@ const app = {
     Register,
     UserProfile,
     Students
+  },
+  methods: {
+    /**
+     * Logs out the current user and redirects to the login page.
+     * Currently does nothing with managing cookies, this needs to be implemented later.
+     */
+    logOut() {
+      this.$router.push('/login');
+    }
   },
   // app initial state
   // https://vuejs.org/v2/guide/instance.html#Data-and-Methods
