1b46b94af4c99c6381f2306e6f6faf22342dfcb6
Eric Song <eso27@uclive.ac.nz>
2021-03-13 17:13:20 +1300

    S302T20-69 #time 15m Hide navbar when in the login or register pages. ?implement ?document #review

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,6 @@
 <template>
   <b-container id="app">
-    <b-navbar toggleable="lg" type="dark" variant="dark" fixed="top">
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
-
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item-dropdown right>
-            <template #button-content>
-              <em>User</em>
-            </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item @click="logOut">Log Out</b-dropdown-item>
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
+    <Navbar/>
     <router-view/>
   </b-container>
 </template>
@@ -24,7 +8,7 @@
 <script>
 import Login from "./components/Login";
 import Register from "./components/Register";
-import Students from "./components/Students";
+import Navbar from "./components/Navbar";
 import UserProfile from "./components/UserProfile";
 
 
@@ -40,16 +24,7 @@ const app = {
     Login,
     Register,
     UserProfile,
-    Students
-  },
-  methods: {
-    /**
-     * Logs out the current user and redirects to the login page.
-     * Currently does nothing with managing cookies, this needs to be implemented later.
-     */
-    logOut() {
-      this.$router.push('/login');
-    }
+    Navbar
   },
   // app initial state
   // https://vuejs.org/v2/guide/instance.html#Data-and-Methods
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <b-navbar v-if="!['login', 'register'].includes($route.name)"
+    toggleable="lg" type="dark" variant="dark" fixed="top"
+  >
+    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown right>
+          <template #button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item @click="logOut">Log Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+/**
+ * A navbar for the site that contains a brand link and navs to user profile and logout.
+ * Will not be shown if is current in the login or register routes. This is done by checking
+ * that the names (not paths!) of the current route are not 'login' or 'register'. Names of routes
+ * are set in router/index.js
+ */
+export default {
+  name: "Navbar.vue",
+  methods: {
+    /**
+     * Logs out the current user and redirects to the login page.
+     * Currently does nothing with managing cookies, this needs to be implemented later.
+     */
+    logOut() {
+      this.$router.push('/login');
+    }
+  },
+}
+</script>
