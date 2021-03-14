2ec397713c023333d172c6196e4728f8e211fa05
Eric Song <eso27@uclive.ac.nz>
2021-03-14 17:44:23 +1300

    S302T20-72 #time 30m Fixing issues with merge commits dissapearing ?fix

--- frontend/src/components/UserProfile.vue
+++ frontend/src/components/UserProfile.vue
@@ -68,6 +68,9 @@ Date: 5/3/2021
 </template>
 
 <script>
+import api from "../Api";
+import usersInfo from './usersDate.json';
+
 export default {
   data: function () {
     return {
@@ -84,23 +87,35 @@ export default {
       }
     }
   },
+
+  mounted() {
+    const userId = this.$route.params.id;
+    this.getUserInfo(userId);
+  },
+
   methods: {
+    /**
+     * this is a get api which can take Specific user to display on the page
+     * The function id means user's id, if the serve find -
+     * -the user's id will response the data and keep the data into this.userData
+     */
+    getUserInfo: function (id) {
+      api
+          .getUser(id) //
+          .then((response) => {
+            this.$log.debug("Data loaded: ", response.data);
+            this.userData = response.data;
+          })
+          .catch((error) => {
+            this.$log.debug(error);
+            this.error = "Failed to Load User Date"
+          })
+      // fake the Api data from the response data.
+      // TESTING PURPOSES ONLY, REMOVE THIS WHEN THE BACKEND IS IMPLEMENTED
+      this.userData = usersInfo.users[id];
+    },
     logOut: function () {
       this.$router.push({path: '/login'})
-    }
-  },
-  watch: {
-    /**
-     * When the user navigates from /user/foo to /user/bar, this component is re-used. This watches for those routing
-     * changes, and will update the user profile with the data of the user specified by the new route.
-     * See https://router.vuejs.org/guide/essentials/dynamic-matching.html#reacting-to-params-changes for more info
-     */
-    /* The argument _from is not needed, so this is to stop eslint complaining:*/
-    /* eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }] */
-    $route(to, _from) {
-      console.log(to);
-      const userId = to.params.id;
-      this.getUserInfo(userId);
     },
     /**
      * Takes a starting and ending date, then calculates the integer number of years and months elapsed since that date.
@@ -121,9 +136,23 @@ export default {
       }
     },
   },
+  watch: {
+    /**
+     * When the user navigates from /user/foo to /user/bar, this component is re-used. This watches for those routing
+     * changes, and will update the user profile with the data of the user specified by the new route.
+     * See https://router.vuejs.org/guide/essentials/dynamic-matching.html#reacting-to-params-changes for more info
+     */
+    /* The argument _from is not needed, so this is to stop eslint complaining:*/
+    /* eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }] */
+    $route(to, _from) {
+      console.log(to);
+      const userId = to.params.id;
+      this.getUserInfo(userId);
+    },
+  },
   computed: {
     memberSince: function() {
-      let registeredDate = new Date(this.userDetail.created);
+      let registeredDate = new Date(this.userData.created);
       const timeElapsed = this.getMonthsAndYearsBetween(registeredDate, Date.now());
       const registeredYears = timeElapsed.years;
       const registeredMonths = timeElapsed.months;
