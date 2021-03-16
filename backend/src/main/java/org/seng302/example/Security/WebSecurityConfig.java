738e4455b9e122200b329b43fd6755f895a1722e
Oliver Cranshaw <ojc31@uclive.ac.nz>
2021-03-16 15:46:22 +1300

    S302T8-23 #time 4h 30m Implementing and debugging /login endpoint to inlcude set-cookie header. ?implement ?fix

--- backend/src/main/java/org/seng302/example/Security/WebSecurityConfig.java
+++ backend/src/main/java/org/seng302/example/Security/WebSecurityConfig.java
@@ -1,37 +1,41 @@
 package org.seng302.example.Security;
 
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.util.CookieGenerator;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                    .antMatchers("/users", "/", "/login", "/h2/**").permitAll()
-                    //.anyRequest().authenticated()
+                    .antMatchers("/users", "/login*", "/h2/**").permitAll()
+                        //.anyRequest().authenticated()
                     .and()
-                    .headers().frameOptions().disable()
-                    .and()
-                .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
-                    .and()
-                .logout()
-                    .permitAll();
+                        .headers().frameOptions().disable()
+                .and()
+                    .formLogin().disable()
+//                        .loginPage("/login.html")
+//                        .loginProcessingUrl("/login")
+//                        .failureUrl("/login.html?error=true")
+//
+//                    .logout().deleteCookies("JSESSIONID")
+                ;
     }
 
-    /*
     @Bean
     @Override
     public UserDetailsService userDetailsService() {
@@ -43,5 +47,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         .build();
 
         return new InMemoryUserDetailsManager(user);
-    } */
+    }
+
 }
