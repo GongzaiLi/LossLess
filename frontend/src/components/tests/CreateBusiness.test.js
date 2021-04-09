d9f5f10c56bf66390cd9ec72f9a650565d4a6c95
Nitish <nsi60@uclive.ac.nz>
2021-04-09 13:17:45 +1200

    S302T700 #time 5m Fixed a bug with a (commented out) test. ?fix ?test

--- frontend/src/components/tests/CreateBusiness.test.js
+++ frontend/src/components/tests/CreateBusiness.test.js
@@ -19,7 +19,6 @@ let createBusiness;
 
 beforeEach(() => {
     createBusiness = jest.fn();
-    // getBusinessData = jest.fn();
 
 
     wrapper = mount(CreateBusiness, {
@@ -103,81 +102,81 @@ describe('CreateBusiness', () => {
 
 
 // HTML TESTING ----------------------------------------------------------------
-// describe('CreateBusiness', () => {
-//     test('Page heading renders', () => {
-//         const headingExpected = "Create a Business";
-//         expect(wrapper.find('h1').text()).toEqual(headingExpected);
-//     });
-// });
-//
-//
-// describe('CreateBusiness', () => {
-//     test('Business Name label renders', () => {
-//         const headingExpected = "Business name *";
-//         expect(wrapper.findAll("b").at(0).text()).toEqual(headingExpected);
-//     });
-// });
-//
-// describe('CreateBusiness', () => {
-//     test('Description label renders', () => {
-//         const headingExpected = "Description";
-//         expect(wrapper.findAll("b").at(1).text()).toEqual(headingExpected);
-//     });
-// });
-//
-// describe('CreateBusiness', () => {
-//     test('Business Address label renders', () => {
-//         const headingExpected = "Business Address *";
-//         expect(wrapper.findAll("b").at(2).text()).toEqual(headingExpected);
-//     });
-// });
-//
-// describe('CreateBusiness', () => {
-//     test('Business Type label renders', () => {
-//         const headingExpected = "Business Type *";
-//         expect(wrapper.findAll("b").at(3).text()).toEqual(headingExpected);
-//     });
-// });
-//
-// describe('CreateBusiness', () => {
-//     test('business types in drop down has default value Choose ...', () => {
-//         const headingExpected = "Choose ...";
-//         expect(wrapper.findAll("select option").at(0).text()).toEqual(headingExpected);
-//     });
-// });
-//
-// describe('CreateBusiness', () => {
-//     test('business types in drop down includes Accommodation and Food Services', () => {
-//         const headingExpected = "Accommodation and Food Services";
-//         expect(wrapper.findAll("select option").at(1).text()).toEqual(headingExpected);
-//     });
-// });
-//
-// describe('CreateBusiness', () => {
-//     test('business types in drop down includes  Retail Trade', () => {
-//         const headingExpected = "Retail Trade";
-//         expect(wrapper.findAll("select option").at(2).text()).toEqual(headingExpected);
-//     });
-// });
-//
-// describe('CreateBusiness', () => {
-//     test('business types in drop down includes Charitable organisation', () => {
-//         const headingExpected = "Charitable organisation";
-//         expect(wrapper.findAll("select option").at(3).text()).toEqual(headingExpected);
-//     });
-// });
-//
-// describe('CreateBusiness', () => {
-//     test('business types in drop down includes Non-profit organisation', () => {
-//         const headingExpected = "Non-profit organisation";
-//         expect(wrapper.findAll("select option").at(4).text()).toEqual(headingExpected);
-//     });
-// });
-//
-//
-// describe('CreateBusiness', () => {
-//     test('Create button renders', () => {
-//         const headingExpected = "Create";
-//         expect(wrapper.find("b-button").text()).toEqual(headingExpected);
-//     });
-// });
+describe('CreateBusiness', () => {
+    test('Page heading renders', () => {
+        const headingExpected = "Create a Business";
+        expect(wrapper.find('h1').text()).toEqual(headingExpected);
+    });
+});
+
+
+describe('CreateBusiness', () => {
+    test('Business Name label renders', () => {
+        const name = "Business name *";
+        expect(wrapper.findAll("b").at(0).text()).toEqual(name);
+    });
+});
+
+describe('CreateBusiness', () => {
+    test('Description label renders', () => {
+        const description = "Description";
+        expect(wrapper.findAll("b").at(1).text()).toEqual(description);
+    });
+});
+
+describe('CreateBusiness', () => {
+    test('Business Address label renders', () => {
+        const address = "Business Address *";
+        expect(wrapper.findAll("b").at(2).text()).toEqual(address);
+    });
+});
+
+describe('CreateBusiness', () => {
+    test('Business Type label renders', () => {
+        const type = "Business Type *";
+        expect(wrapper.findAll("b").at(3).text()).toEqual(type);
+    });
+});
+
+describe('CreateBusiness', () => {
+    test('business types in drop down has default value Choose ...', () => {
+        const noOption = "Choose ...";
+        expect(wrapper.findAll("select option").at(0).text()).toEqual(noOption);
+    });
+});
+
+describe('CreateBusiness', () => {
+    test('business types in drop down includes Accommodation and Food Services', () => {
+        const option1 = "Accommodation and Food Services";
+        expect(wrapper.findAll("select option").at(1).text()).toEqual(option1);
+    });
+});
+
+describe('CreateBusiness', () => {
+    test('business types in drop down includes  Retail Trade', () => {
+        const option2 = "Retail Trade";
+        expect(wrapper.findAll("select option").at(2).text()).toEqual(option2);
+    });
+});
+
+describe('CreateBusiness', () => {
+    test('business types in drop down includes Charitable organisation', () => {
+        const option3 = "Charitable organisation";
+        expect(wrapper.findAll("select option").at(3).text()).toEqual(option3);
+    });
+});
+
+describe('CreateBusiness', () => {
+    test('business types in drop down includes Non-profit organisation', () => {
+        const option4 = "Non-profit organisation";
+        expect(wrapper.findAll("select option").at(4).text()).toEqual(option4);
+    });
+});
+
+
+describe('CreateBusiness', () => {
+    test('Create button renders', () => {
+        const buttonLabel = "Create";
+        expect(wrapper.find("button").text()).toEqual(buttonLabel);
+    });
+});
