2e3a777ed3fe9a1c4c3e6ea910217d410a4ebb2b
Gongzai Li <gli65@uclive.ac.nz>
2021-04-15 00:10:30 +1200

    S302T700-32 #time 20m Create Catalogue page with default prompt to select business ?implement -1. I built a router to default business '/businesses/:id/products' -2. built a page about ProductCatalogue.vue

---
+++ frontend/src/components/ProductCatalogue.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>Product Catalogue</h2>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
