47ab0ad3d73f683ad3001ec3f2e2bb774c9fd8ac
Arish Abalos <ama541@uclive.ac.nz>
2021-04-16 10:18:53 +1200

    S302T700-8 #time 15m Changing test timer to be more efficient ?test
    - Changed timer for thread.sleep to be 15000 instead of matching the config as this may cause a huge wait time on testing

--- backend/src/test/java/com/seng302/wasteless/ScheduledTaskIntegrationTest.java
+++ backend/src/test/java/com/seng302/wasteless/ScheduledTaskIntegrationTest.java
@@ -4,29 +4,23 @@ import com.seng302.wasteless.service.DefaultAdminCreatorService;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@PropertySource("classpath:global-admin.properties")
 @AutoConfigureMockMvc
 public class ScheduledTaskIntegrationTest {
 
     @Autowired
     DefaultAdminCreatorService creatorService;
 
-    @Value("${check-default-admin-period-seconds}")
-    private int wait_time;
-
     @Test
     public void checkIfScheduledMethodIsCalledWithCreatorService() throws InterruptedException{
-        Thread.sleep(wait_time + 10000);
+        Thread.sleep(15000);
         assertEquals(creatorService.getInvocationCount() > 0, true);
     }
 }
