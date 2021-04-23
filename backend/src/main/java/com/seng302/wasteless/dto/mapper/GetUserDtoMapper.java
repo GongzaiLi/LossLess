8452cf38553a7ce4a55cb9ffdf07bee7e5c4c80d
Nitish <nsi60@uclive.ac.nz>
2021-04-23 14:28:58 +1200

    S302T700-54 #time 3h Current user role checked before returning USER DATA on get request. ?implement

--- backend/src/main/java/com/seng302/wasteless/dto/mapper/GetUserDtoMapper.java
+++ backend/src/main/java/com/seng302/wasteless/dto/mapper/GetUserDtoMapper.java
@@ -1,11 +1,12 @@
 package com.seng302.wasteless.dto.mapper;
 
 import com.seng302.wasteless.dto.GetUserDto;
-import com.seng302.wasteless.model.Business;
-import com.seng302.wasteless.model.BusinessAdministered;
-import com.seng302.wasteless.model.User;
+import com.seng302.wasteless.model.*;
 import com.seng302.wasteless.service.BusinessService;
+import com.seng302.wasteless.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -21,6 +22,12 @@ import java.util.List;
 public class GetUserDtoMapper {
 
     private static BusinessService businessService;
+    private static UserService userService;
+    private static Login login;
+
+
+
+
 
     @Autowired
     public GetUserDtoMapper(BusinessService businessService) {
@@ -30,6 +37,25 @@ public class GetUserDtoMapper {
     public static GetUserDto toGetUserDto(User user) {
 
         List<Business> businesses = businessService.findBusinessesByUserId(user.getId());
+
+        //UserRoles currentUserRole = userService.findUserByEmail(login.getEmail()).getRole(); //get the role of Currently logged in user
+
+        User tempUser = new User();    //just for testing. Implement this later ...
+        tempUser.setRole(UserRoles.USER); //changing this should change the data that is returned
+        tempUser.setEmail("admin@700");
+        tempUser.setPassword("password");
+
+        UserRoles currentUserRole = tempUser.getRole();
+
+
+
+
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        String currentPrincipalEmail = authentication.getName();
+//
+//        User loggedInUser = userService.findUserByEmail(currentPrincipalEmail);
+//        UserRoles currentUserRole = loggedInUser.getRole();
+
 
         List<BusinessAdministered> businessesAdministered = new ArrayList<>();
 
@@ -52,8 +78,9 @@ public class GetUserDtoMapper {
                         .setCreated(business.getCreated().toString())
             );
         }
+        GetUserDto getUserDto = new GetUserDto();
 
-        return new GetUserDto()
+        getUserDto
                 .setId(user.getId())
                 .setFirstName(user.getFirstName())
                 .setLastName(user.getLastName())
@@ -64,9 +91,18 @@ public class GetUserDtoMapper {
                 .setDateOfBirth(user.getDateOfBirth().toString())
                 .setPhoneNumber(user.getPhoneNumber())
                 .setHomeAddress(user.getHomeAddress())
-                .setCreated(user.getCreated().toString())
-                .setRole(user.getRole())
+                .setCreated(user.getCreated().toString());
+
+        if (currentUserRole.equals(UserRoles.GLOBAL_APPLICATION_ADMIN) ||
+                currentUserRole.equals(UserRoles.DEFAULT_GLOBAL_APPLICATION_ADMIN)) {
+            getUserDto
+                    .setRole(user.getRole());
+
+        }
+        getUserDto
                 .setBusinessesAdministered(businessesAdministered);
+
+        return getUserDto;
 
     }
 }
