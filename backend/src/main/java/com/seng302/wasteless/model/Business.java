a1b35f5f9633ec54af5315aa1c27da3336a99fc5
Arish Abalos <ama541@uclive.ac.nz>
2021-04-29 22:35:03 +1200

    S302T700-71 #time 2h Implemented /businesses/{id}/makeAdministrator" endpoint ?implement ?pair[ama541, jeh128]
    - Implemented revoke admin from a business endpoint
    - Changed PutBusinessMakeAdminDto to a PutBusinessAdminDto as it is used by both RevokeAdmin and MakeAdmin endpoints
    - Created method inside service removeAdministratorFromBusiness
    - Created inside model removeAdmin

--- backend/src/main/java/com/seng302/wasteless/model/Business.java
+++ backend/src/main/java/com/seng302/wasteless/model/Business.java
@@ -61,4 +61,6 @@ public class Business {
     public void addAdministrator(User user) {
         this.administrators.add(user);
     }
+
+    public void removeAdministrator(User user) {this.administrators.remove(user);}
 }
