9d5732e6aa661199b3e3d2c33982b17debd78d7e
Eric Song <eso27@uclive.ac.nz>
2021-05-04 14:12:19 +1200

    Fix sonarqube jest test setup ?fix ?chore

--- frontend/sonarqube-analysis.js
+++ frontend/sonarqube-analysis.js
@@ -10,7 +10,7 @@ sonarqubeScanner(
             'sonar.sources': 'src',
             'sonar.tests': 'src/tests',
             'sonar.inclusions': '**',
-            'sonar.test.inclusions': 'src/**/*.spec.js,src/**/*.tests.js,src/**/*.tests.ts',
+            'sonar.test.inclusions': 'src/**/*.spec.js,src/**/*.test.js,src/**/*.test.ts',
             'sonar.typescript.lcov.reportPaths': 'coverage/lcov.info',
             'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
             'sonar.testExecutionReportPaths': 'coverage/test-reporter.xml'
