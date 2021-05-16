54a6abd3f0bae42a9773adbd5f107f9b94375905
Nitish <nsi60@uclive.ac.nz>
2021-05-16 13:19:04 +1200

    Added ListingsController and DTOs with documentation

---
+++ backend/src/main/java/com/seng302/wasteless/dto/PostListingsDto.java
@@ -0,0 +1,50 @@
+package com.seng302.wasteless.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+import javax.persistence.Column;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+
+/**
+ * Dto for post inventory endpoint.
+ */
+
+@Accessors(chain = true) //Allows chaining of getters and setters
+@Data // generate setters and getters for all fields (lombok pre-processor)
+@NoArgsConstructor // generate a no-args constructor needed by JPA (lombok pre-processor)
+@ToString // generate a toString method
+public class PostListingsDto {
+
+   
+
+    @NotBlank(message = "Inventory item ID is Mandatory")
+    private Long inventoryItemId;
+
+    @Positive
+    @Max(1000000000)            //max = inventoryItem.quantity()
+    @Min(1)
+    @NotNull(message = "Inventory item quantity is Mandatory")
+    @Column(name = "quantity")
+    private int quantity;
+
+    @PositiveOrZero
+    @Max(1000000000)
+    @Column(name = "price")
+    private double price;
+
+    @Column(name = "moreInfo")
+    private String moreInfo;
+
+    @PastOrPresent                     // default = inventory item expiry  ONLY present
+    // default = inventory item expiry
+    @Column(name = "created")
+    private LocalDate created;
+
+    @Future                     // default = inventory item expiry
+    @Column(name = "closes")
+    private LocalDate closes;
+}
---
+++ backend/src/main/java/com/seng302/wasteless/dto/mapper/PostListingsDtoMapper.java
@@ -0,0 +1,47 @@
+package com.seng302.wasteless.dto.mapper;
+
+import com.seng302.wasteless.dto.PostInventoryDto;
+import com.seng302.wasteless.dto.PostListingsDto;
+import com.seng302.wasteless.model.Inventory;
+import com.seng302.wasteless.model.Listings;
+import com.seng302.wasteless.model.Product;
+import com.seng302.wasteless.service.InventoryService;
+import com.seng302.wasteless.service.ProductService;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Column;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Component
+public class PostListingsDtoMapper {
+
+    private static InventoryService inventoryService;
+
+    @Autowired
+    public PostListingsDtoMapper(InventoryService inventoryService) {
+        PostListingsDtoMapper.inventoryService = inventoryService;
+    }
+
+    public static Listings postListingsDto(PostListingsDto listingsDto) {
+
+        Inventory inventory = inventoryService.findInventoryById(listingsDto.getInventoryItemId());
+
+
+        Listings listings = new Listings();
+        listings.setInventoryItem(inventory);
+        listings.setQuantity(listingsDto.getQuantity());
+        listings.setPrice(listingsDto.getPrice());
+        listings.setMoreInfo(listingsDto.getMoreInfo());
+        listings.setCreated(listingsDto.getCreated());
+        listings.setCloses(listingsDto.getCloses());
+
+        ;
+
+        return listings;
+    }
+
+
+}
