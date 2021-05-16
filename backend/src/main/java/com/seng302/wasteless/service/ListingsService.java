1c0e0839710c28154b360cf92a5ecec263333230
Nitish <nsi60@uclive.ac.nz>
2021-05-16 13:54:27 +1200

    S302T700-94 #time 5m Fixed ListingsService to pass failing pipeline. ?fix

--- backend/src/main/java/com/seng302/wasteless/service/ListingsService.java
+++ backend/src/main/java/com/seng302/wasteless/service/ListingsService.java
@@ -2,7 +2,9 @@ package com.seng302.wasteless.service;
 
 import com.seng302.wasteless.dto.mapper.PostListingsDtoMapper;
 import com.seng302.wasteless.model.Listings;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ListingsService {
 
     public Listings createListing(Listings listingItem) {
