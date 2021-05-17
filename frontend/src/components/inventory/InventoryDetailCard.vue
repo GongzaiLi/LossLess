b5ba65711e2501c10e610dd52b16aa6c7a7718a9
Gongzai Li <gli65@uclive.ac.nz>
2021-05-17 17:51:27 +1200

    S302T700-120 #time 1h 48m implement Link modify inventory to  "Edit" card in a similar way to modify catalogue ?implement ?testmanual #review 1. refectory the ok button now when click the ok button will check the modal first. (if modal is edit will go to edit api function otherwise go to create api function). 2. Inventory table has edit button when click the edit button the edit modal will show up. 3. manual testing https://docs.google.com/spreadsheets/d/1yG5dr8mgaGWslX_0oNjLFXb0jDuM9naEJBRPQxSLJSs/edit#gid=0

--- frontend/src/components/inventory/InventoryDetailCard.vue
+++ frontend/src/components/inventory/InventoryDetailCard.vue
@@ -7,7 +7,7 @@ Date: 13/5/2021
     <b-card
       class="profile-card shadow"
     >
-      <b-form @submit="createInventory">
+      <b-form @submit="okAction">
         <div :hidden="!disabled">
           <b-img center v-bind="mainProps" rounded="circle" alt="Default Image"></b-img>
         </div>
@@ -47,7 +47,7 @@ Date: 13/5/2021
               :disabled="disabled"
               @input="calculateTotalPrice"
               v-model="inventoryInfo.pricePerItem"
-              />
+            />
             <template #append>
               <b-input-group-text>{{ currency.code }}</b-input-group-text>
             </template>
@@ -65,7 +65,7 @@ Date: 13/5/2021
               step=".01" min=0 placeholder=0
               :disabled="disabled"
               v-model="inventoryInfo.totalPrice"
-              />
+            />
             <template #append>
               <b-input-group-text>{{ currency.code }}</b-input-group-text>
             </template>
@@ -135,7 +135,7 @@ Date: 13/5/2021
 
 <style>
 
-#select-products>.modal-dialog {
+#select-products > .modal-dialog {
   max-width: 1000px;
 }
 
@@ -179,7 +179,7 @@ export default {
   },
   data() {
     return {
-      mainProps: { blank: true, blankColor: '#777', width: 150, height: 150, class: 'm1' },
+      mainProps: {blank: true, blankColor: '#777', width: 150, height: 150, class: 'm1'},
       inventoryInfo: {},
       inventoryCardError: ""
     }
@@ -192,7 +192,7 @@ export default {
      * init the data which is from the inventory table
      */
     setUpInventoryCard() {
-      this.inventoryInfo = this.inventory;
+      this.inventoryInfo = Object.assign({}, this.inventory);
       if (this.disabled) {
         this.inventoryInfo.productId = this.inventoryInfo.product.id.split(/-(.+)/)[1];
         this.inventoryInfo.manufactured = this.setDate(this.inventoryInfo.manufactured);
@@ -206,7 +206,9 @@ export default {
      * set date to a string form.
      **/
     setDate(date) {
-      if (date != null)  {return new Date(date).toUTCString().split(' ').slice(0, 4).join(' ')}
+      if (date != null) {
+        return new Date(date).toUTCString().split(' ').slice(0, 4).join(' ')
+      }
     },
 
     /**
@@ -224,16 +226,14 @@ export default {
     async createInventory(event) {
       event.preventDefault();
       await api
-          .createInventory(this.$route.params.id, this.inventoryInfo)
-          .then((createInventoryResponse) => {
-            this.$log.debug("Inventory Created", createInventoryResponse);
-            this.$bvModal.hide('inventory-card');
-            this.setUpInventoryPage();
-          })
-          .catch((error) => {
-            this.inventoryCardError = this.getErrorMessageFromApiError(error);
-            this.$log.debug(error);
-          });
+        .createInventory(this.$route.params.id, this.inventoryInfo)
+        .then((createInventoryResponse) => {
+          this.$log.debug("Inventory Created", createInventoryResponse);
+        })
+        .catch((error) => {
+          this.inventoryCardError = this.getErrorMessageFromApiError(error);
+          this.$log.debug(error);
+        });
     },
 
     /**
@@ -278,6 +278,27 @@ export default {
       this.inventoryInfo.productId = product.id;
       this.$bvModal.hide('select-products');
     },
+    /**
+     * Ok action button when edit modal is ture
+     * the OkAction will modify the Inventory otherwise Create a Inventory
+     */
+    okAction: async function () {
+      if (this.editModal) {
+        await this.editInventory();
+      } else {
+        await this.createInventory();
+      }
+      this.$bvModal.hide('inventory-card');
+      this.setUpInventoryPage();// update the table
+    },
+
+    /**
+     * Edit Inventory APi
+     * @return {Promise<void>}
+     */
+    editInventory: async function () {
+      //todo edit the inventory api in here...................
+    }
   }
 }
 </script>
