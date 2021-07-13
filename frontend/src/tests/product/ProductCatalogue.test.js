063e8af79a92d726288f04fb5833ad936202cfe7
Eric Song <eso27@uclive.ac.nz>
2021-07-13 12:35:07 +1200

    S302T700-185 #time 1h 10m Send API request when primary image button clicked ?implement ?document ?test

--- frontend/src/tests/product/ProductCatalogue.test.js
+++ frontend/src/tests/product/ProductCatalogue.test.js
@@ -117,8 +117,9 @@ describe('Testing api put/post request and the response method with errors', () 
 
   it('Create product but receives 413 (image) error ', async () => {
     Api.createProduct.mockResolvedValue({response : {status: 201}, data: {productId: '51-A'}});
-    Api.uploadProductImages.mockRejectedValue({response : {status: 413, data: "Image larger than 5MB"}});
+    Api.uploadProductImage.mockRejectedValue({response : {status: 413, data: "Image larger than 5MB"}});
 
+    wrapper.vm.productDisplayedInCard.images = [{filename: 'blah'}];
     await wrapper.vm.createProduct();
 
     expect(wrapper.vm.productCardError).toBe("Image larger than 5MB");
