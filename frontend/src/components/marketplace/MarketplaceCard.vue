2138128d99d86f8a59ddd2a04859b77ed73097a8
Eric Song <eso27@uclive.ac.nz>
2021-07-20 17:59:20 +1200

    S302T700-193 #time 5m Truncate card description to three lines ?implement ?document

--- frontend/src/components/marketplace/MarketplaceCard.vue
+++ frontend/src/components/marketplace/MarketplaceCard.vue
@@ -1,24 +1,46 @@
 <template>
     <b-card
         :title="cardInfo.title"
-        style="height: 96%"
+        class="mt-3"
     >
-      <b-card-body>
-        <b-card-text>
-          <h6>{{cardInfo.description}}</h6>
-        </b-card-text>
-        <b-card-text>
-          Tags: {{ formatTags }}
-        </b-card-text>
-        <b-card-text>
-          Seller: {{cardInfo.creator.firstName}} {{cardInfo.creator.lastName}}
-        </b-card-text>
-        <b-card-text>
-          Location: {{ formatAddress }}
-        </b-card-text>
-      </b-card-body>
+      <b-card-text class="marketplace-card-short-description truncate-fade">
+        <h6 style="line-height: 1.2em;">{{cardInfo.description}}</h6>
+      </b-card-text>
+      <b-card-text>
+        Tags: {{ formatTags }}
+      </b-card-text>
+      <b-card-text>
+        Seller: {{cardInfo.creator.firstName}} {{cardInfo.creator.lastName}}
+      </b-card-text>
+      <b-card-text>
+        Location: {{ formatAddress }}
+      </b-card-text>
     </b-card>
 </template>
+
+<style scoped>
+/*
+This clamps the descriptions to three liens with a nice fade effect when truncated
+See https://css-tricks.com/line-clampin/#the-fade-out-way for how this works
+*/
+.marketplace-card-short-description {
+  overflow: hidden;
+}
+.truncate-fade {
+  position: relative;
+  height: 3.8em;
+}
+.truncate-fade:after {
+  content: "";
+  text-align: right;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  width: 60%;
+  height: 1.2em;
+  background: linear-gradient(to right, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1) 80%);
+}
+</style>
 
 <script>
 
