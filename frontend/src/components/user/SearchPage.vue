76e15fcf904a285fe835ae2ec06859d8eacc270d
Arish Abalos <ama541@uclive.ac.nz>
2021-08-05 10:57:29 +1200

    S302T700-321 #time 40m Implement generic search page, implement tabs on generic search page to contain user search ?implement ?pair[ama541, ojc31]

--- frontend/src/components/user/SearchPage.vue
+++ frontend/src/components/user/SearchPage.vue
@@ -1,14 +1,28 @@
 <template>
-  <b-tabs v-model=activeTabIndex align="center" fill>
-    <b-tab v-for="[section, sectionName] in sections" :key=section :title=sectionName>
-
+  <b-card class="shadow" style="max-width: 85%">
+    <h1><b-icon-search/> Search </h1>
+    <b-tabs v-model=activeTabIndex align="center" fill style="padding-top: 16px">
+    <b-tab title="User" :title-item-class="'tab-title-class'">
+      <user-search>
+      </user-search>
+    </b-tab>
+    <b-tab title="Business" :title-item-class="'tab-title-class'">
     </b-tab>
   </b-tabs>
+  </b-card>
 </template>
 
+<style>
+
+.tab-title-class {
+  font-size: 24px;
+}
+</style>
+
 <script>
+import UserSearch from "./UserSearch";
 export default {
-  components: {},
+  components: {UserSearch},
   data: function () {
     return {
       sections: [['UserSearch', 'User Search'], ['BusinessSearch', 'Business Search']],
