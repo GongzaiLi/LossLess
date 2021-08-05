92fc24aa0f4398e79dd0ef8c703c85091ce12fc0
Eric Song <eso27@uclive.ac.nz>
2021-08-05 15:03:47 +1200

    S302T700-371 #time 40m Add endpoint to search listings by partial matches ?implement ?document

--- backend/src/main/java/com/seng302/wasteless/dto/GetListingDto.java
+++ backend/src/main/java/com/seng302/wasteless/dto/GetListingDto.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonView;
 import com.seng302.wasteless.model.Listing;
 import com.seng302.wasteless.view.ListingViews;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.Accessors;
 
@@ -16,7 +15,6 @@ import java.util.List;
  */
 @Accessors(chain = true) //Allows chaining of getters and setters
 @Data // generate setters and getters for all fields (lombok pre-processor)
-@NoArgsConstructor // generate a no-args constructor needed by JPA (lombok pre-processor)
 @ToString // generate a toString method
 public class GetListingDto {
 
@@ -24,5 +22,5 @@ public class GetListingDto {
     private List<Listing> listings;
 
     @JsonView(ListingViews.GetListingView.class)
-    private Integer totalItems;
+    private Long totalItems;
 }
