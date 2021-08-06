f8473da20ba844373021b139f1e3a37a86acbdae
Nitish Singh <nsi60@uclive.ac.nz>
2021-08-06 14:11:07 +1200

    S302-T700-399 #time 3h Added styling to search and filter elements based on the wireframe. Added method to get current date as string with docstring. Added a small fix to notifactions to prevent clicking on invisible notification.  ?document ?fix ?implement ?pair[nsi60, pwt25]

--- frontend/src/components/listing/ListingsSearchPage.vue
+++ frontend/src/components/listing/ListingsSearchPage.vue
@@ -3,43 +3,61 @@
   <b-card style="max-width: 80em;">
     <b-container>
       <h1>Search for Listings</h1>
-      <div align-h="start">
-        <b-row>
+      <hr>
+      <div>
+
+        <b-row >
           <b-col cols="5">
-            <b-input placeholder="Search: Product Name" v-model="search.productName"></b-input>
+            <div class="input-group mb-2 mr-sm-2">
+              <div class="input-group-prepend">
+                <div class="input-group-text">Search:</div>
+              </div>
+              <b-input placeholder="Product Name" v-model="search.productName"></b-input>
+            </div>
           </b-col>
+
+
           <b-col cols="4">
-            <b-select v-model="search.sort" value="inventoryItem.product.name">
-              <option value="nameAsc">Sort: Product Name A to Z</option>
-              <option value="nameDesc">Product Name Z to A</option>
-              <option value="businessNameAsc">Seller Name A to Z</option>
-              <option value="businessNameDesc">Seller Name Z to A</option>
-              <option value="priceAsc">Price Low To High</option>
-              <option value="priceDesc">Price High To Low</option>
-              <option value="closesAsc">Listing Closes Earlier to Later</option>
-              <option value="closesDesc">Listing Closes Later to Earlier</option>
-              <option value="expiryAsc">Expiry Date Earlier to Later</option>
-              <option value="expiryDesc">Expiry Date Later to Earlier</option>
-              <option value="countryAsc">Location A to Z</option>
-              <option value="countryDesc">Location A to Z</option>
-            </b-select>
+            <div class="input-group mb-2 mr-sm-2">
+              <div class="input-group-prepend">
+                <div class="input-group-text">Sort:</div>
+              </div>
+              <b-select v-model="search.sort" value="inventoryItem.product.name">
+                <option value="nameAsc">Product Name A to Z</option>
+                <option value="nameDesc">Product Name Z to A</option>
+                <option value="businessNameAsc">Seller Name A to Z</option>
+                <option value="businessNameDesc">Seller Name Z to A</option>
+                <option value="priceAsc">Price Low To High</option>
+                <option value="priceDesc">Price High To Low</option>
+                <option value="closesAsc">Listing Closes Earlier to Later</option>
+                <option value="closesDesc">Listing Closes Later to Earlier</option>
+                <option value="expiryAsc">Expiry Date Earlier to Later</option>
+                <option value="expiryDesc">Expiry Date Later to Earlier</option>
+                <option value="countryAsc">Location A to Z</option>
+                <option value="countryDesc">Location A to Z</option>
+              </b-select>
+            </div>
+
           </b-col>
-          <b-col>
+
+          <b-col class="search_button" cols="">
             <b-button @click="doSearch">Search</b-button>
           </b-col>
-          <b-col>
+          <b-col class="search_button" cols="1">
             <b-button v-b-toggle.collapse-1 variant="primary">Filter</b-button>
           </b-col>
 
         </b-row>
+        <hr>
         <b-collapse id="collapse-1" class="mt-2">
         <b-row>
+
           <b-col>
             <b-input placeholder="Business Name" v-model="search.businessName"></b-input>
           </b-col>
           <b-col>
-            <b-select placeholder="Business Type" v-model="search.businessType">
-              <option value=""> Business Type</option>
+            <b-select  v-model="search.businessType" >
+              <option :value="null"> Business Type</option>
               <option> Accommodation and Food Services</option>
               <option> Retail Trade</option>
               <option> Charitable organisation</option>
@@ -51,24 +69,25 @@
           </b-col>
 
         </b-row>
-        <b-row>
-          <b-col>
-            <b-input-group>
-              <b-input type="date" placeholder="Date Min:" style="width: 5rem" v-model="search.closesStartDate"></b-input>
-              <label style="margin-left: 1rem;margin-right: 1rem"> to </label>
-              <b-input type="date" placeholder="Date: Max" v-model="search.closesEndDate" style="width: 5rem"></b-input>
-            </b-input-group>
+          <br>
+        <b-row align-h="between">
+          <b-col cols="7">
+              <b-input-group prepend="Listing Close:">
+                <b-input type="date" v-model="search.closesStartDate"></b-input>
+                <label style="margin-left: 1rem;margin-right: 1rem;"> to </label>
+                <b-input type="date" v-model="search.closesEndDate"> </b-input>
+              </b-input-group>
+
           </b-col>
-          <b-col>
-            <b-input-group>
-              <b-input type="number" placeholder="Price Min:" style="width: 5rem" v-model="search.priceMin"></b-input>
+          <b-col cols="4" >
+            <b-input-group prepend="Price:" >
+              <b-input type="number" placeholder="Min:" v-model="search.priceMin"></b-input>
               <label style="margin-left: 1rem;margin-right: 1rem"> to </label>
-              <b-input type="number" placeholder="Price Max"  style="width: 5rem" v-model="search.priceMax"></b-input>
+              <b-input type="number" placeholder="Max"  v-model="search.priceMax"></b-input>
             </b-input-group>
           </b-col>
         </b-row>
-
-
+          <hr>
         </b-collapse>
       </div>
 
@@ -106,6 +125,14 @@
   </b-card>
 </template>
 
+<style>
+
+.search_button {
+  text-align: right;
+}
+
+</style>
+
 <script>
 import pagination from "../model/Pagination";
 import api from "../../Api";
@@ -123,7 +150,7 @@ name: "ListingsSearchPage",
         productName:"",
         sort:"nameAsc",
         businessName:"",
-        businessType:"",
+        businessType:null,
         businessLocation:"",
         closesStartDate: "",
         closesEndDate:"",
@@ -142,9 +169,20 @@ name: "ListingsSearchPage",
   },
   mounted() {
     this.initListingPage();
+    this.search.closesStartDate = this.getToday()
   },
 
   methods: {
+    /**
+     * Get today's date without the time
+     * need to add one to get correct date
+     * @return today's date in format yyyy-mm-dd
+     **/
+    getToday() {
+      let date = new Date();
+      return date.getFullYear() + "-" + (date.getMonth() + 1).toString().padStart(2, '0') + '-' + date.getDate().toString().padStart(2, '0');
+    },
+
     /**
      * Page initilisation function
      **/
@@ -161,7 +199,6 @@ name: "ListingsSearchPage",
         await api.getUserCurrency(card.business.address.country)
             .then((response) => {
               card.business.currency = response;
-              console.log(this.cards[0].business.currency)
             })
             .catch((error) => {
               this.$log.debug(error);
