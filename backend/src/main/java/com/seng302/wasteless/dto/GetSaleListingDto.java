61aee0a4af6ba998397bd24d3da5b40a224d17b4
Oliver Cranshaw <ojc31@uclive.ac.nz>
2021-08-09 11:29:25 +1200

    S302T700-380 #time 1h Adjusted the dto for sale listings and created a dto mapper to map the listing and business info into one dto object. ?implement

--- backend/src/main/java/com/seng302/wasteless/dto/GetSaleListingDto.java
+++ backend/src/main/java/com/seng302/wasteless/dto/GetSaleListingDto.java
@@ -1,22 +1,18 @@
 package com.seng302.wasteless.dto;
 
-import com.fasterxml.jackson.annotation.JsonView;
-import com.seng302.wasteless.dto.mapper.GetUserDtoMapper;
-import com.seng302.wasteless.model.*;
-import com.seng302.wasteless.view.ListingViews;
+import com.seng302.wasteless.dto.mapper.GetSaleListingDtoMapper;
+import com.seng302.wasteless.model.Address;
+import com.seng302.wasteless.model.Business;
+import com.seng302.wasteless.model.Inventory;
+import com.seng302.wasteless.model.Listing;
+import com.seng302.wasteless.service.BusinessService;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.Accessors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-import javax.persistence.Column;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.validation.constraints.*;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
 
 
 /**
@@ -24,13 +20,14 @@ import java.util.stream.Collectors;
  * No DTO mapper is needed as the constructor takes care of everything.
  */
 
+
 @Accessors(chain = true) //Allows chaining of getters and setters
 @Data // generate setters and getters for all fields (lombok pre-processor)
 @ToString // generate a toString method
-public class GetSaleListingDTO {
+public class GetSaleListingDto {
 
     private Integer id;
-    private GetInventoryDto inventoryItem;
+    private Inventory inventoryItem;
     private int quantity;
     private double price;
     private String moreInfo;
@@ -40,12 +37,5 @@ public class GetSaleListingDTO {
     private String businessName;
     private Address businessAddress;
 
-    /**
-     * Creates a new DTO from a Listing entity
-     * @param listing The Listing entity to create this DTO from
-     */
-    public GetSaleListingDTO(Listing listing) {
-
-    }
 
 }
