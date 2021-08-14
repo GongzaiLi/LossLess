5c2348c5a1b45d342e75e8ba52deb73a3124347d
Gongzai Li <gli65@uclive.ac.nz>
2021-08-14 13:25:54 +1200

    S302T700-318 ?time 1h 1m Create PurchasedListings entity type selector ?implement ?fix #review - add Purchased Listing entity - Fix bug where wrong in sql data

---
+++ backend/src/main/java/com/seng302/wasteless/model/PurchasedListing.java
@@ -0,0 +1,59 @@
+package com.seng302.wasteless.model;
+
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Data // generate setters and getters for all fields (lombok pre-processor)
+@NoArgsConstructor // generate a no-args constructor needed by JPA (lombok pre-processor)
+@Entity // declare this class as a JPA entity (that can be mapped to a SQL table)
+@Accessors(chain = true) //Allows chaining of getters and setters
+public class PurchasedListing {
+
+    @Id // this field (attribute) is the table primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "business")
+    private Business business;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "user")
+    private User user;
+
+
+    @NotNull
+    @JoinColumn(name = "sale_date")
+    private LocalDate saleDate;
+
+
+    @NotNull
+    @JoinColumn(name = "listing_date")
+    private LocalDate listingDate;
+
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "product")
+    private Product product;
+
+
+    @NotNull
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    @NotNull
+    @Column(name = "price")
+    private Double price;
+
+}
+
