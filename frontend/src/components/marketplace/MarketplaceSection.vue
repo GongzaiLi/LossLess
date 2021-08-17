9d9e04491b061353fcadef6d278ef34b3ec6b1fd
Gongzai Li <gli65@uclive.ac.nz>
2021-08-17 16:30:33 +1200

    S302T700-424 #time 25m fix cards in table view cannot be sorted by date #35 ?fix

--- frontend/src/components/marketplace/MarketplaceSection.vue
+++ frontend/src/components/marketplace/MarketplaceSection.vue
@@ -97,7 +97,6 @@ export default {
   components: {pagination, MarketplaceCard, MarketplaceCardFull},
   props: ["isCardFormat", "cardsPerRow", "perPage", "section"],
   mounted() {
-    console.log(this.cardsPerRow);
     this.refreshData();
   },
   data: function () {
@@ -138,6 +137,16 @@ export default {
           label: "Location",
           sortable: true
         },
+        {
+          key: 'created',
+          label: "created",
+          sortable: true,
+          formatter: (value) => {
+            const date = new Date(value).toUTCString();
+            return date.split(" ").slice(1, 5).join(" ");
+          },
+        }
+
       ]
     }
   },
