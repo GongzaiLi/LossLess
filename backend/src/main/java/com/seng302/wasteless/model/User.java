6cc38da09204634cb045bb7e8b616619a6ef412a
Caleb Sim <cws47@uclive.ac.nz>
2021-08-17 12:15:58 +1200

    S302T700-420 #time 30m changed business administrated to not be eager ?ifx

--- backend/src/main/java/com/seng302/wasteless/model/User.java
+++ backend/src/main/java/com/seng302/wasteless/model/User.java
@@ -92,7 +92,8 @@ public class User {
     private String password;
 
     @Column(name = "businesses_primarily_administered")
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany
+    @LazyCollection(LazyCollectionOption.FALSE)
     private List<Business> businessesPrimarilyAdministered;
 
     @Column(name = "created") // map camelcase name (java) to snake case (SQL)
