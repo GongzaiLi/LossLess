ed2a9e213ccb25f3f150acbaa1e03ffdb6c7676d
pwt25 <pwt25@uclive.ac.nz>
2021-09-19 15:03:34 +1200

    S302T700-473 #time 1h implemented message entity and boilerplates for controller repository and service ?implement ?pair[pwt25, ama541]

---
+++ backend/src/main/java/com/seng302/wasteless/service/MessageService.java
@@ -0,0 +1,10 @@
+package com.seng302.wasteless.service;
+
+import org.springframework.stereotype.Service;
+/**
+ * MessageService applies message logic over the card JPA repository.
+ */
+@Service
+public class MessageService {
+}
+
