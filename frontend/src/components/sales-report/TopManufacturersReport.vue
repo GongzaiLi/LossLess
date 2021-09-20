758532808e6e02f94b0e706eafa42e26d8508f66
Oliver Cranshaw <ojc31@uclive.ac.nz>
2021-09-20 12:01:37 +1200

    S302T700-602 #time 35m Added manufacturers graph to the extended sales report. ?implement

--- frontend/src/components/sales-report/TopManufacturersReport.vue
+++ frontend/src/components/sales-report/TopManufacturersReport.vue
@@ -35,6 +35,7 @@
 
 import Api from "../../Api";
 import {formatDate} from "../../util";
+import Chart from "chart.js/auto";
 
 export default {
   name: "top-manufacturers-report",
@@ -64,7 +65,49 @@ export default {
   async mounted() {
     this.businessId = this.$route.params.id;
     await this.getManufacturersReport(this.dateRange);
+    const cfg = {
+      type: 'doughnut',
+      data: {
+        datasets: [{
+          label: 'Top Products By Quantity',
+          data: this.results.map(record => record.totalProductPurchases),
+          backgroundColor: ['#1105c1', '#0e2fcb', '#0c4ad2', '#0a62d8', '#0979dd', '#078fe3', '#06a5e8', '#04bbee', '#03d1f3', '#01e8f9', '#00feff']        ,
+        },],
+        labels: this.results.map(record => record.manufacturer)
+      },
+      options: {
+        elements: {
+          arc: {
+            spacing: 3
+          }
+        },
+        plugins: {
+          tooltip: {
+            callbacks: {
+              label: (t) => {
+                const labels = [
+                  ` ${t.label}`,
+                  ` Total Quantity Sold: ${this.results[t.dataIndex].totalProductPurchases}`,
+                  ` Total Value Sold: ${this.currency.symbol}${this.results[t.dataIndex].totalValue}`,
+                  ` Total Likes: ${this.results[t.dataIndex].totalLikes}`
+                ];
 
+                if (this.doughnutOption === 'totalValue') {
+                  [labels[1], labels[2]] = [labels[2], labels[1]];
+                } else if (this.doughnutOption === 'totalLikes') {
+                  [labels[1], labels[3]] = [labels[3], labels[1]];
+                }
+
+                return labels;
+              },
+            }
+          },
+        },
+      }
+    };
+    this.chart = new Chart(
+        document.getElementById('top-manufacturers-graph').getContext('2d'),
+        cfg);
   },
 
   methods: {
@@ -85,6 +128,15 @@ export default {
           });
     },
 
+    /**
+     * Updates the chart data when different options are selected
+     */
+    updateChart: function() {
+      this.chart.data.datasets[0].data = this.results.map((record) => record[this.doughnutOption]);
+      this.chart.data.labels = this.results.map(record => record.product.id.split(/-(.+)/)[1]);
+
+      this.chart.update();
+    }
   }
 }
 
