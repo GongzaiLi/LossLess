ff387da4e2bd68a41e64ffb8dd8e8079c5600d0b
Eric Song <eso27@uclive.ac.nz>
2021-09-22 16:49:24 +1200

    S302T700-613 #time 27m Display histogram values when hovering over bars ?implement

--- frontend/src/components/sales-report/ListingsDurationsGraph.vue
+++ frontend/src/components/sales-report/ListingsDurationsGraph.vue
@@ -1,5 +1,8 @@
 <template>
-  <canvas id="listings-durations-graph"></canvas>
+  <div>
+    <h3 class="mt-3 w-100 text-center">Durations between sale and closing dates</h3>
+    <canvas id="listings-durations-graph"></canvas>
+  </div>
 </template>
 
 <script>
@@ -20,7 +23,6 @@ export default {
       type: 'bar',
       data: {
         datasets: [{
-          label: 'Durations',
           data: newData,
           backgroundColor: ['#0f8d39'],
         }]
@@ -30,7 +32,6 @@ export default {
         scales: {
           xAxis: {
             type: 'linear',
-            display: true,
             offset: false,
             grid: {
               offset: false
@@ -38,8 +39,40 @@ export default {
             ticks: {
               stepSize: 1,
             },
+            title: {
+              display: true,
+              text: "Number of days between sale date and closing date",
+              font: {
+                size: 16
+              }
+            }
           },
-        }
+          yAxis: {
+            title: {
+              display: true,
+              text: "Number of listings sold",
+              font: {
+                size: 16
+              }
+            }
+          }
+        },
+        plugins: {
+          legend: {
+            display: false
+          },
+          tooltip: {
+            callbacks: {
+              title: (t) => {
+                const daysValue = t[0].parsed.x;
+                return ` Sold ${Math.floor(daysValue)} - ${Math.ceil(daysValue)} days before closing`;
+              },
+              label: (t) => {
+                return ` ${t.parsed.y} listings`;
+              }
+            }
+          },
+        },
       }
     };
     this.chart = new Chart(
